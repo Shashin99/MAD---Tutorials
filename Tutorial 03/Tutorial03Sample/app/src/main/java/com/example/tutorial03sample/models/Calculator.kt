37fb50bbf41be195a7362c4847c7364b48988a4e
K L Shashin Kalpajith <127762218+Shashin99@users.noreply.github.com>
2023-06-09 09:49:51 +0530

    update

--- Tutorial 03/Tutorial03Sample/app/src/main/java/com/example/tutorial03sample/models/Calculator.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tutorial03sample.models
-
-class Calculator (private val number1:Double, private val number2:Double){
-    fun add() = number1 + number2
-    fun subtract() = number1 - number2
-    fun divide() = number1 * number2
-    fun multiply() = number1 / number2
-}
